99453117cd7f3698857bae920fde13f74fc26a36
Peter Lemenkov <lemenkov@gmail.com>
2026-06-21 10:03:44 +0200

    REUSE compliance
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<!--
+SPDX-FileCopyrightText: 2026 Peter Lemenkov <lemenkov@gmail.com>
+SPDX-License-Identifier: Apache-2.0
+-->
+
 # mcp-koji
--- src/mcp_koji/__init__.py
+++ src/mcp_koji/__init__.py
@@ -1,1 +1,4 @@
+# SPDX-FileCopyrightText: 2026 Peter Lemenkov <lemenkov@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
 """MCP server for Fedora Koji."""
